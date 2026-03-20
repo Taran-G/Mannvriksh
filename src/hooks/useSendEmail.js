83e669dda647b41b382555feb3ea76612d3308e6
Karan grewal <karangrewal979@gmail.com>
2026-03-21 00:03:07 +0530

    add send email functionality on form submit by making a custom useSendEmail.js hook and doing changes in BookingModal.jsx

--- src/hooks/useSendEmail.js
+++ src/hooks/useSendEmail.js
@@ -1,7 +1,25 @@
 import { useRef } from "react";
+import emailjs from '@emailjs/browser';
 
+ function useSendEmail() { 
+     const formRef = useRef();
+     const sendEmail = (e) => {
+        e.preventDefault();
 
-function useSendEmail() {
-    const 
-    export const form = useRef();
+        emailjs
+        .sendForm('service_msack18', 'template_6pwsmrc', formRef.current, {
+        publicKey: 'DaPXYRYJippOSe6bc',
+      })
+      .then(        
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        }
+      );
+
+     }
+     return {formRef, sendEmail};
 }
+export default useSendEmail;
